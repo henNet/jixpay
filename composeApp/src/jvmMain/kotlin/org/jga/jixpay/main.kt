3df86f3dcf6b5801eaa26268ca1cc19e761d9118
henNet <dmf.n123@gmail.com>
2025-10-14 18:49:25 -0300

    feat: implementada a funcionalidade do Sim

--- composeApp/src/jvmMain/kotlin/org/jga/jixpay/main.kt
+++ composeApp/src/jvmMain/kotlin/org/jga/jixpay/main.kt
@@ -5,13 +5,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -19,16 +19,29 @@ import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import java.lang.module.ModuleFinder
+import androidx.compose.runtime.*
+import jixpay.composeapp.generated.resources.Res
+import jixpay.composeapp.generated.resources.pix
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun MainScreen(){
+
+    var isYesPressed by remember { mutableStateOf(false) }
+
     MaterialTheme {
         Box(modifier = Modifier.fillMaxSize().background(Color.LightGray),
             contentAlignment = Alignment.Center) {
-            Text("E o jix nada?", Modifier.offset(y = -50.dp))
+            Text("E o Jix nada?", Modifier.offset(y = -50.dp))
 
-            Button(onClick = {}, modifier = Modifier.offset(x = -50.dp)) {
+            Button(onClick = {isYesPressed = true},
+                modifier = Modifier.offset(x = -50.dp)
+                ) {
                 Text("Sim")
+            }
+
+            Button(onClick = {}, modifier = Modifier.offset(x = 50.dp)) {
+                Text("Não")
             }
         }
     }
@@ -38,6 +51,7 @@ fun main() {
     application {
         Window(onCloseRequest = ::exitApplication,
             title = "JixPay",
+            icon = painterResource(Res.drawable.pix),
             state = WindowState(
                 position = WindowPosition(Alignment.Center),
                 size = DpSize(800.dp, 600.dp
